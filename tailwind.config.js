f6a9455ec39d78426212f729f4c899ee6ebb5c1a
David Kellerman <david@silvermoon.org>
2024-08-18 14:19:09 -0400

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{jsx,tsx,css}"],
+  content: ["./src/**/*.{jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    // require('@tailwindcss/typography'),
     require('tailwindcss-animate'),
   ],
 };
